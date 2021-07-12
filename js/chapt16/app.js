99ba40fd74f29c0eca1ebc90326d0bcc4d146f7e
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-07-11 23:57:20 -0300

    JS pt2

--- js/chapt16/app.js
+++ js/chapt16/app.js
@@ -1,3 +1,21 @@
-console.log("Hello from my first app.js")
-let total = 2 + 2
-console.log("Bye")
+// let idadeUser = prompt("Type your age")
+
+// if (idadeUser >= 18){
+//     console.log("maior de idade")
+//     alert("Ok, you are an adult")
+// }
+// else{
+//     console.log("menor de idade")
+//     alert("Calm down kid")
+// }
+
+// numero random gerado de 1 ao 10
+let numeroRandom = Math.floor(Math.random() * 10) + 1
+    if (numeroRandom <= 5){
+        console.log("numero MENOR da metade: " + numeroRandom)
+        alert("numero MENOR da metade: " + numeroRandom)
+    }
+    else{
+        console.log("numero MAIOR da metade: " + numeroRandom)
+        alert("numero MAIOR da metade: " + numeroRandom)
+    }
