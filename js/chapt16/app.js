011d190595a940daaedf4798d342e75bae320d70
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-07-15 00:02:28 -0300

    exercicios

--- js/chapt16/app.js
+++ js/chapt16/app.js
@@ -1,3 +1,4 @@
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // let idadeUser = prompt("Type your age")
 
 // if (idadeUser >= 18){
@@ -8,19 +9,41 @@
 //     console.log("menor de idade")
 //     alert("Calm down kid")
 // }
-
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // numero random gerado de 1 ao 10
 //let numeroRandom = Math.floor(Math.random() * 10) + 1
-let numeroRandom = prompt("Digite sua nota, de 1 a 10")
-    if (numeroRandom >= 6){
-        console.log("Vc esta APROVADO: " + numeroRandom)
-        alert("Vc atingiu o minimo pra passar: " + numeroRandom)
+// let numeroRandom = prompt("Digite sua nota, de 1 a 10")
+//     if (numeroRandom >= 6){
+//         console.log("Vc esta APROVADO: " + numeroRandom)
+//         alert("Vc atingiu o minimo pra passar: " + numeroRandom)
+//     }
+//     else if (numeroRandom >= 3 && numeroRandom < 6 ){
+//         console.log("Vc esta de REC: " + numeroRandom)
+//         alert("Vc esta de REC: " + numeroRandom)
+//     }
+//     else{
+//         console.log("Vc esta REPROVADO: " + numeroRandom)
+//         alert("Vc esta REPROVADO: " + numeroRandom)
+//     }
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    //const teste = prompt("Digite algo pra ver se eh TRUTHY oR FALSY")
+// TRUTHY = Qualquer coisa diferente de FALSY
+// FALSY = 0, "", FALSE, NaN, Undefinied
+   
+//    if(teste){  // Esse IF pergunta se ta VAZIO
+    //if(123){ // sempre TRUTH , se fosse IF(0) seria sempre FALSE, pois 0 = FALSE
+    //if(0){  // sempre FALSE
+    //if(NaN){ //sempre FALSE. Sempre fosse Undefinied tbm
+    let variavel = " "; // se for 0, "", NaN, Undefinied... cai FALSY
+
+     if(variavel){
+        alert("TRUTHY") 
+        console.warn("Good")
     }
-    else if (numeroRandom >= 3 && numeroRandom < 6 ){
-        console.log("Vc esta de REC: " + numeroRandom)
-        alert("Vc esta de REC: " + numeroRandom)
-    }
-    else{
-        console.log("Vc esta REPROVADO: " + numeroRandom)
-        alert("Vc esta REPROVADO: " + numeroRandom)
-    }
+     else{
+         alert("FALSY")
+         console.error("Nooo")
+     }
+
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
