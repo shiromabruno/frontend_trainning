2908d0bbf80dce2bf7c33c18e2e2a411fbc4c3f6
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-08-02 22:27:30 -0300

    exercises

---
+++ js/chapt16/loops/app2.js
@@ -0,0 +1,13 @@
+
+const assuntos = ["futebol", "games", "tecnologia", "carreira", "teste"];
+
+//dessa forma printa o conteudo
+for(let sub of assuntos){
+    console.log(sub)
+}
+
+
+//dessa forma printa o INDEX
+for(let sub in assuntos){
+    console.log(sub)
+}
