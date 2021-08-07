831e26ed6f3573151d4a3c1eef657162db880274
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-08-07 00:34:47 -0300

    lexical scope

---
+++ js/chapt21/app.js
@@ -0,0 +1,22 @@
+function funcaoFora(){
+    let teste = ["listaFora1", 5, true, null]
+
+    function funcaoDentro1(){
+        let teste2 = ["listaFora2", 10, false, NaN]
+
+        function funcaoDentro2(){
+
+            for(let variavel of teste){
+                console.log(`printando lista: ${variavel}` )
+            }
+
+            for(let variavel2 of teste2){
+                console.log(`printando lista2: ${variavel2}` )
+            }
+
+        }
+        funcaoDentro2()
+    }
+    
+    funcaoDentro1()
+}
