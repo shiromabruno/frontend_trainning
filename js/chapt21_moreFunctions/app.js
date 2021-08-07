720e94cd0462f85852438f607b08f0e088f3518c
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-08-07 19:21:18 -0300

    returning functions

--- js/chapt21_moreFunctions/app.js
+++ js/chapt21_moreFunctions/app.js
@@ -1,3 +1,5 @@
+//a FUNCTION dentro de FUNCTION meio que protege as FUNCTION de dentro. Apenas a Function mais externas consegue chamar
+//Exemplo: chamando no console o funcaoDentro1() ou funcaoDentro2(). Apenas funcaoFora() fuciona
 function funcaoFora(){
     let teste = ["listaFora1", 5, true, null]
 
@@ -25,6 +27,13 @@ const addAlgo = function (x,y){
     return x+y;
 }
 
+// CRIAR NO CONSOLE:  let dadosAleatorios = function (){return Math.floor(Math.random()*6) + 1}
+// CHAMA dadosAleatorios no chamadorFuncaoTemplate(func) abaixo
+
+function chamadorFuncaoTemplate(func){
+    console.log(func());
+}
+
 function chamaFuncaoSemParametro(){
     console.log(`Resultado da funcao eh: ${addAlgo(8,20)}`)
 }
@@ -36,4 +45,30 @@ function chamaFuncaoComParametroFuncao(func){
 
 function chamaFuncaoComParametroValores(x, y){
     console.log(`Resultado da funcao eh: ${addAlgo(x,y)}`)
+}
+// -==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--
+// -==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--==-=--
+
+function operacaoCalculadora(operacao){
+    switch(operacao){
+        case "soma":
+            return function soma(x, y){ return x+y }
+        case "subtracao":
+            return function subtracao(x, y){ return x-y }
+        case "divisao":
+            return function divisao(x, y){ return x/y }
+        case "multiplicacao":
+            return function multiplicacao(x, y){ return x*y }
+    }
+}
+
+function chamadorFuncaoCalculadora(nome, x, y){
+    nomeDoMetodo = operacaoCalculadora(nome);
+    //console.log(nomeDoMetodo(10,30));
+    chamadorTemplateCalculadora(nomeDoMetodo, x, y);
+    console.log(`Resultado do chamadorFuncaoCalculadora: ${nomeDoMetodo(x,y)}`)
+}
+
+function chamadorTemplateCalculadora(func,x,y){
+    console.log( `Resultado do chamadorTemplateCalculadora: ${func(x,y)}`);
 }
