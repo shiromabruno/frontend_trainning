ac15c179a305718bfc7f968c06d77dfb5572688a
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-08-09 22:20:43 -0300

    methods

---
+++ js/chapt21_moreFunctions/METHODSAndObjects/app.js
@@ -0,0 +1,24 @@
+let objetoTeste = {
+nome: "bruno",
+age: 25,
+brazillian: true,
+valor1: 5,
+valor2: 4,
+calculatorSoma: function(x,y){ return x + y},
+calculatorSubtracao: function(x,y){ return x-y},
+multiply(x,y){return x * y},
+division(x,y){return x/y}
+}
+
+/* 
+objetoTeste["testando"] = 5
+5
+objetoTeste.calculatorSoma(5,3)
+8
+typeof(objetoTeste)
+"object"
+typeof(Math)
+"object"
+objetoTeste.multiply(5,8)
+40
+*/
