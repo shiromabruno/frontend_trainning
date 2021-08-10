0086f441383ddc436caa3e9e3e4495681c05c92f
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-08-09 23:08:53 -0300

    try catch

--- js/chapt21_moreFunctions/try_catch/app.js
+++ js/chapt21_moreFunctions/try_catch/app.js
@@ -1,53 +1,25 @@
-let objetoTeste = {
-nome: "bruno",
-familyname: "shiroma",
-age: 25,
-brazillian: true,
-valor1: 5,
-valor2: 4,
-calculatorSoma: function(x,y){ return x + y},
-calculatorSubtracao: function(x,y){ return x-y},
-multiply(x,y){return x * y},
-division(x,y){return x/y},
-nomecompleto: `Nome completo eh: ${this.nome} ${this.familyname}`,
-funcaoAtributoNomeCompleto: function(){ return `Nome completo eh: ${this.nome} ${this.familyname}`},
-howOldAreYou(){ console.log("minha idade eh: " + this.age)},
-whatsYourName(){console.log(this.nome)},
-retornoObjeto: function(){return this}
+
+try{
+hello.toUpperCase();
+}
+catch{
+console.log("Error, nao conseguiu deixar maiusculo")
 }
 
-let objetonovo = objetoTeste.howOldAreYou;
+function somaNada(){
+    try{
+    console.log(x + y);
+    }
+    catch(e){
+        console.log(`Error while executing somaNada function. ERROR: ${e}`)
+    }
+}
 
-let objetonovo2 = objetoTeste.retornoObjeto;
-
-function grito(){ console.log("AAAHHHHHH")}
-/*
-grito();
-app.js:23 AAAHHHHHH
-window.grito();
-app.js:23 AAAHHHHHH
-*/
-function grit2(){ return "EEEHHHH"}
-/*
-window.grit2();
-"EEEHHHH"
-*/
-
-/* 
-objetoTeste["testando"] = 5
-5
-objetoTeste.calculatorSoma(5,3)
-8
-typeof(objetoTeste)
-"object"
-typeof(Math)
-"object"
-objetoTeste.multiply(5,8)
-40
-
-objetoTeste.howOldAreYou()
-app.js:14 minha idade eh: 25
-objetonovo()
-app.js:14 minha idade eh: undefined
-*/
-
+function somaAlgo(x,y){
+    try{
+        return (x + y);
+    }
+    catch(e){
+        console.log(`Error while executing somaAlgo function. ERROR: ${e}`)
+    }
+}
