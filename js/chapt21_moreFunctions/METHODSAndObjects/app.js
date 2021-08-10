5f20d4c2f426e671f576282d8804793cd6b5803f
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-08-09 22:44:24 -0300

    methods in objects and this

--- js/chapt21_moreFunctions/METHODSAndObjects/app.js
+++ js/chapt21_moreFunctions/METHODSAndObjects/app.js
@@ -1,5 +1,6 @@
 let objetoTeste = {
 nome: "bruno",
+familyname: "shiroma",
 age: 25,
 brazillian: true,
 valor1: 5,
@@ -7,8 +8,17 @@ valor2: 4,
 calculatorSoma: function(x,y){ return x + y},
 calculatorSubtracao: function(x,y){ return x-y},
 multiply(x,y){return x * y},
-division(x,y){return x/y}
+division(x,y){return x/y},
+nomecompleto: `Nome completo eh: ${this.nome} ${this.familyname}`,
+funcaoAtributoNomeCompleto: function(){ return `Nome completo eh: ${this.nome} ${this.familyname}`},
+howOldAreYou(){ console.log("minha idade eh: " + this.age)},
+whatsYourName(){console.log(this.nome)},
+retornoObjeto: function(){return this}
 }
+
+let objetonovo = objetoTeste.howOldAreYou;
+
+let objetonovo2 = objetoTeste.retornoObjeto;
 
 /* 
 objetoTeste["testando"] = 5
@@ -21,4 +31,10 @@ typeof(Math)
 "object"
 objetoTeste.multiply(5,8)
 40
+
+objetoTeste.howOldAreYou()
+app.js:14 minha idade eh: 25
+objetonovo()
+app.js:14 minha idade eh: undefined
 */
+
