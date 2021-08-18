70c21f7d9eb5451b89a8b9749ba01f24806b8a21
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-08-17 23:16:28 -0300

    arrow

---
+++ js/chapt22_arrayMethods/arrow.js
@@ -0,0 +1,24 @@
+let calculoNumeroDouble = (x) => {
+    return x * 2;
+}
+
+//OU
+
+let calculoNumeroDouble2 = function dobra (x){
+    return x * 2;
+}
+
+let calculoDoisNumerosx = (x,y) => {
+    return x * y;
+}
+
+let calculoDoisNumerosx2 = function dobra (x,y){
+    return x * y;
+}
+
+let listaNumbers = [1,2,3,4,5,6];
+let somaTotal = 0;
+listaNumbers.forEach(element => {
+    somaTotal =  somaTotal + element;
+})
+
