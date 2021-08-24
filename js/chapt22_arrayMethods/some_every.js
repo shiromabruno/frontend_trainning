f06e726a9c1711a65ee8b9e18f232cdf8aa293e2
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-08-23 23:08:39 -0300

    some and every

---
+++ js/chapt22_arrayMethods/some_every.js
@@ -0,0 +1,56 @@
+//SOME RETORNA TRUE OR FALSE NO FINAL
+//SOME RETORNA TRUE OR FALSE NO FINAL
+//SOME RETORNA TRUE OR FALSE NO FINAL
+//ATENDEU A CONDICAO, JA SAI DO SOME (Achou manchester, parou de comparar)
+
+
+
+let teams = ["barcelona", "mancherster united", "bayern", "real madrid", "milan", "juventus"]
+
+
+let existeTimeMaior10Charact = teams.some(team => {
+    if(team.length > 10){
+        return team;
+    }
+})
+
+console.log("A resposta se a lista tem elementos com mais de 10 elements eh: " + existeTimeMaior10Charact)
+
+// 0-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=
+
+let viagens = [
+    {
+        pais: "japao",
+        preco: 20000
+    },
+    {
+        pais: "inglaterra",
+        preco: 30000
+    },
+    {
+        pais: "italia",
+        preco: 15000
+    },
+    {
+        pais: "brazil",
+        preco: 10000
+    },
+    {
+        pais: "south africa",
+        preco: 11000
+    }
+    ]
+
+let viagemMenorQue10 = viagens.some(viagem => {
+    if (viagem.preco < 10000){
+        return viagem
+    }
+})
+
+console.log("A resposta se a lista de viagem tem viagem menor que 10k: " + viagemMenorQue10)
+
+// 0-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=
+
+//Execise retorno so de pares
+
+let allEvens = (x) => x.some(num => num % 2 === 0)
