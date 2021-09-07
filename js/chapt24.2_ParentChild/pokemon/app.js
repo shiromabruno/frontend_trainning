84d279f7a096710945e6b929aeabb45d4e662925
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-09-07 19:55:03 -0300

    pokemon exercise

---
+++ js/chapt24.2_ParentChild/pokemon/app.js
@@ -0,0 +1,19 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+const container = document.querySelector('#container');
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+
+for (let i = 1; i <= 151; i++) {
+    const pokemonDiv = document.createElement('div');
+    pokemonDiv.classList.add('pokemonClass');
+    const label = document.createElement('span');
+    label.innerText = `#${i}`;
+    const newImg = document.createElement('img');
+    newImg.src = `${baseURL}${i}.png`
+
+
+    pokemonDiv.appendChild(newImg);
+    pokemonDiv.appendChild(label);
+    container.appendChild(pokemonDiv)
+}
