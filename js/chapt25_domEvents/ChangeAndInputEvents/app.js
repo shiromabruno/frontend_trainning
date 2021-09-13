346ad83eb5947b84a845364efb33b73aad0dbeb6
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-09-13 11:30:32 -0300

    input and change

--- js/chapt25_domEvents/ChangeAndInputEvents/app.js
+++ js/chapt25_domEvents/ChangeAndInputEvents/app.js
@@ -1,10 +1,17 @@
-const input = document.querySelector('input');
-const h1 = document.querySelector('h1');
+let input = document.querySelector('input');
+let h1 = document.querySelector('h1')
 
-// input.addEventListener('change', function (e) {
-//     console.log("CASKDJASKJHD")
-// })
+//change funciona , nesse caso, quando vc preenche algo no textfield e leave o textfield com a alteracao
+//se vc so escrever coisa e nao sair do text field, nada ocorre. Se vc escreveru algo e saiu. Acontece a fucntion. Se vc entrar de novo, nao alterar nada e sair, nada ocorre [pois nao houve alteracao]
+//mesmo q "tenha alteracao" ,  mas vc apagou e voltou o valor de novo, nao houve alteracao nesse caso. Exemplo: tinha dog. Ai vc entrou e agaou o "g" e colocou de novo o "g" e saiu. Nao houve alteracao
+input.addEventListener('change', function(e){
+    console.log('houve change')
+})
 
-input.addEventListener('input', function (e) {
+//no input, sempre quando digita algo no textfiled, ativa a function. Mesmo alterando ou nao o conteudo.
+// somente CTRL + C nao conta como input, mas se fizer ctrol+V ai sim conta
+input.addEventListener('input', function(e){
+    //console.log('houve input')
     h1.innerText = input.value;
 })
+
