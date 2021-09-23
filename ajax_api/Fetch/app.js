b17ca40596c276c2437e5f8a8aff2325d9824715
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-09-22 23:10:38 -0300

    fetch api

--- ajax_api/Fetch/app.js
+++ ajax_api/Fetch/app.js
@@ -1,3 +1,37 @@
+// fetch('https://api.cryptonator.com/api/ticker/btc-usd') // return a Promise
+// .then(res => {
+//     console.log("Resposta antes do PARSE JSON: ", res); // o RES eh um objeto, entao nao da pra ter o "+" console.log("Resposta: " + res);
+//     console.log("o RES eh um objeto, entao nao da pra ter o '+' console.log('Resposta: ' + res);");
+//     return res.json(); // o return caira no .THEN abaixo. PS: poderia estar o THEN linkado ja nessa linha... [res.json().then...]
+// })
+// .then(data => {
+//     console.log("Informacao ja com PARSE do JSON: ", data)
+//     console.log("Fazendo o data.ticker.price [ticker eh um dos atributos]: " + data.ticker.price);
+// })
+// .catch(error => {
+//     console.log("Erro: ", error);
+// })
+
+let fetchBitcoinPrice = async () => {
+    try {
+        let resposta = await fetch('https://api.cryptonator.com/api/ticker/btc-usd');
+        //console.log(resposta);
+        let data = await resposta.json();
+        console.log(data.ticker.price);
+
+        // chamando de novo com outra URL
+        let respostabrl = await fetch('https://api.cryptonator.com/api/ticker/btc-brl');
+        //console.log(resposta);
+        let databrl = await respostabrl.json();
+        console.log(databrl.ticker.price);
+    }
+    catch(erro){
+        console.log("Erro na chamada: " + erro);
+    }
+    
+}
+
+
 // fetch('https://api.cryptonator.com/api/ticker/btc-usd')
 //     .then(res => {
 //         console.log("RESPONSE, WAITING TO PARSE...", res)
@@ -12,12 +46,12 @@
 //     })
 
 
-const fetchBitcoinPrice = async () => {
-    try {
-        const res = await fetch('https://api.cryptonator.com/api/ticker/btc-usd');
-        const data = await res.json();
-        console.log(data.ticker.price)
-    } catch (e) {
-        console.log("SOMETHING WENT WRONG!!!", e)
-    }
-}
+// const fetchBitcoinPrice = async () => {
+//     try {
+//         const res = await fetch('https://api.cryptonator.com/api/ticker/btc-usd');
+//         const data = await res.json();
+//         console.log(data.ticker.price)
+//     } catch (e) {
+//         console.log("SOMETHING WENT WRONG!!!", e)
+//     }
+// }
