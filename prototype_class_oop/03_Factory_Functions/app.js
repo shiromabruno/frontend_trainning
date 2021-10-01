52de8b4caa777bade57a1d6dea51bf65c8652a00
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-09-30 22:09:15 -0300

    object

--- prototype_class_oop/03_Factory_Functions/app.js
+++ prototype_class_oop/03_Factory_Functions/app.js
@@ -1,27 +1,50 @@
-//This functions makes and returns an object every time it is called.
-// The resulting objects all follow the same "recipe"
-function makeColor(r, g, b) {
-	const color = {};
+function makeColor(r,g,b){
+	let color = {};
+
 	color.r = r;
 	color.g = g;
 	color.b = b;
-	color.rgb = function() {
-		const { r, g, b } = this;
+	color.rgb = function(){ // o objetivo eh so chamar objetoX.rgb() que ja voltaria o rgb (nao precisa passa r,g,b)
+		let {r,g,b} = this;  // referese ao objeto COLOR
+		//return `rgb(${this.r}, ${this.g}, ${this.b})`; ==:> ao inves de usar essa linha, usar o destruct da linha de cima
 		return `rgb(${r}, ${g}, ${b})`;
 	};
-	color.hex = function() {
-		const { r, g, b } = this;
-		return (
-			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
-		);
-	};
-	return color;
+	color.hex = function() { // o objetivo eh so chamar objetoX.hex() que ja voltaria o hex (nao precisa passa r,g,b)
+		let {r,g,b} = this; // referese ao objeto COLOR
+			return (
+				'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+			);
+		};
+
+	return color; // let teste = makeColor(123,070,100)
 }
 
-const firstColor = makeColor(35, 255, 150);
-firstColor.hex(); //firstColor.hex();
-firstColor.rgb(); //"rgb(35, 255, 150)"
 
-const black = makeColor(0, 0, 0);
-black.rgb(); //"rgb(0, 0, 0)"
-black.hex(); //"#0000s00"
+
+//This functions makes and returns an object every time it is called.
+// The resulting objects all follow the same "recipe"
+// function makeColor(r, g, b) {
+// 	const color = {};
+// 	color.r = r;
+// 	color.g = g;
+// 	color.b = b;
+// 	color.rgb = function() {
+// 		const { r, g, b } = this;
+// 		return `rgb(${r}, ${g}, ${b})`;
+// 	};
+// 	color.hex = function() {
+// 		const { r, g, b } = this;
+// 		return (
+// 			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+// 		);
+// 	};
+// 	return color;
+// }
+
+// const firstColor = makeColor(35, 255, 150);
+// firstColor.hex(); //firstColor.hex();
+// firstColor.rgb(); //"rgb(35, 255, 150)"
+
+// const black = makeColor(0, 0, 0);
+// black.rgb(); //"rgb(0, 0, 0)"
+// black.hex(); //"#0000s00"
