8b6a2bb493945935b930a2f746ce08f32e60c99d
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-10-03 23:09:02 -0300

    creating class

--- prototype_class_oop/05_Classes/app.js
+++ prototype_class_oop/05_Classes/app.js
@@ -1,26 +1,44 @@
 class Color {
-	constructor(r, g, b, name) {
-		this.r = r;
-		this.g = g;
-		this.b = b;
-		this.name = name;
-	}
-	innerRGB() {
-		const { r, g, b } = this;
-		return `${r}, ${g}, ${b}`;
-	}
-	rgb() {
-		return `rgb(${this.innerRGB()})`;
-	}
-	rgba(a = 1.0) {
-		return `rgba(${this.innerRGB()}, ${a})`;
-	}
-	hex() {
-		const { r, g, b } = this;
+    constructor(r, g, b, construtorQualquer){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.colorName = construtorQualquer;
+    }
+
+    // EH UM PROTOTYPE !!!
+    // Color.prototype.greet = function(){ ... }
+    greet(){
+        return `Hello from ${this.colorName} !!!`
+    }
+
+    innerRGB(){
+        let {r, g, b: renomeandoB } = this;
+        return `${r}, ${g}, ${renomeandoB}`;
+    }
+
+    rgb(){
+        //let {r, g, b: renomeandoB } = this;
+        //return `rgb(${r}, ${g}, ${renomeandoB})`;
+        return `rgb(${this.innerRGB()})`
+    }
+
+    // document.body.style.backgroundColor = objeto1.rgba(5)
+    rgba(a = 1.0){ // esse a= 1.0 eh so o DEFAULT. Se passar assim [objeto1.rgba(5)] fica com 5 ===> 'rgb(125, 78, 210, 5)'
+        return `rgb(${this.innerRGB()}, ${a})`
+    }
+
+    //objeto1.hex === objeto2.hex  ----> referencia ao mesmo function no prototype, por isso eh TRUE
+    //true
+    hex(){
+        let {r: renomeandoR, g: renomeandoG ,b} = this; // referese ao objeto COLOR. Cria atributos R e G e B
 		return (
-			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+			'#' + ((1 << 24) + (renomeandoR << 16) + (renomeandoG << 8) + b).toString(16).slice(1)
 		);
 	}
 }
-const red = new Color(255, 67, 89, 'tomato');
-const white = new Color(255, 255, 255, 'white');
+
+let objeto1 = new Color(125, 78, 210, "BrunoShiroma");
+let objeto2 = new Color(023, 197, 115, "green");
+
+//document.body.style.backgroundColor = objeto1.rgba(5)
